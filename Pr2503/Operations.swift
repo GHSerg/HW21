9faa247582942b22b0497baf228c6ea18d871ff6
Serg <your@serg.com>
2022-06-23 11:59:35 +0500

    Create class Password

---
+++ Pr2503/Operations.swift
@@ -0,0 +1,8 @@
+//
+//  Operations.swift
+//  Pr2503
+//
+//  Created by Sergey Bodnar on 23.06.2022.
+//
+
+import Foundation
--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+var password = Password()
+
 class ViewController: UIViewController {
     
     var isBlack: Bool = false {
@@ -13,17 +15,17 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var generateButton: UIButton!
     @IBOutlet weak var passwordFiled: UITextField!
     @IBOutlet weak var indicatorView: UIActivityIndicatorView!
-
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        indicatorView.isHidden = true
         //self.bruteForce(passwordToUnlock: "1!gr")
-        
-        // Do any additional setup after loading the view.
+
     }
     
     //MARK: - Actions
@@ -33,79 +35,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func generate(_ sender: Any) {
-
-        var password = String()
-        
-        password += String(password.digits.randomElement() ?? "0")
-        password += String(password.lowercase.randomElement() ?? "a")
-        password += String(password.uppercase.randomElement() ?? "A")
-        password += String(password.punctuation.randomElement() ?? "!")
-        print(password)
-        self.passwordFiled.text = password
-       // self.bruteForce(passwordToUnlock: password)
-    }
-    
-    //MARK: - Private functions
-    private func bruteForce(passwordToUnlock: String) {
-        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
-
-        var password: String = ""
-
-        // Will strangely ends at 0000 instead of ~~~
-        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-//             Your stuff here
-            print(password)
-            // Your stuff here
-        }
-        
-        print(password)
+        print (password.generatePassword())
+        indicatorView.isHidden = false
+        generateButton.isEnabled = false
+        indicatorView.startAnimating()
     }
 }
 
 
-//MARK: - Extension functions
-extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation }
 
-
-
-    mutating func replace(at index: Int, with character: Character) {
-        var stringArray = Array(self)
-        stringArray[index] = character
-        self = String(stringArray)
-    }
-}
-
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index])
-                               : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
-        }
-    }
-
-    return str
-}
 
